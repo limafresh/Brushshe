b07d847bdbd071638c0b25cb001ff226f7a04359
Maksim <173814432+limafresh@users.noreply.github.com>
2025-06-19 22:09:56 +0300

    Updated create exe script

--- dev_tools/create-portable-exe.ps1
+++ dev_tools/create-portable-exe.ps1
@@ -5,10 +5,10 @@ Write-Host 'Switching to the virtual environment...'
 & .\brenv\Scripts\Activate.ps1
 
 Write-Host 'Installing dependencies...'
-pip install pyinstaller pip-licenses pillow customtkinter
+pip install pyinstaller pip-licenses pillow customtkinter typing-extensions
 
 Write-Host 'Creating a file with licenses...'
-pip-licenses --format=plain-vertical --with-license-file --no-license-path --no-version --from=mixed --output-file=dependencies-licenses.txt
+pip-licenses --format=plain-vertical --with-license-file --no-license-path --no-version --from=mixed --with-system --output-file=dependencies-licenses.txt
 
 Write-Host 'PyInstaller packaging...'
 pyinstaller --noconfirm --onefile --windowed --icon "..\Brushshe\icons\icon.ico" --name "Brushshe_64bit_portable.exe" --add-data "..\Brushshe\brushshe_theme.json;." --add-data "brenv\Lib\site-packages\customtkinter;customtkinter/" --add-data "..\Brushshe\icons;icons/" --add-data "..\Brushshe\locales;locales/" --add-data "..\README.md;." --add-data "..\LICENSE;." --add-data "dependencies-licenses.txt;." --add-data "..\Brushshe\assets;assets/"  "..\Brushshe\brushshe.py"
