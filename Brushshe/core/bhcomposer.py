4449555be3793685ecbf4395a4fd8b7383000d42
Chip Viled <chipviled@gmail.com>
2025-08-11 22:59:48 +0300

    The ants prototype (filter version)

--- Brushshe/core/bhcomposer.py
+++ Brushshe/core/bhcomposer.py
@@ -1,5 +1,5 @@
 # import math
-from PIL import Image, ImageChops, ImageDraw
+from PIL import Image, ImageChops, ImageDraw, ImageFilter
 
 
 class BhComposer:
@@ -19,10 +19,14 @@ class BhComposer:
         self.background_color_2 = "#D0D0D0"
         self.background_size = 256
         self.background_tile_size = 16
+        self.ants_tile_size = 16
         self.background_tile_image = self.generate_tile_image()
+        self.ants_tile_image = self.generate_ants_image()
 
         self.l_image = None
         self.background_image = None
+        self.ants_image = None
+        self.ants_position = 0
 
         self.mask_img = None  # Must be gray image (L mode)
 
@@ -48,6 +52,24 @@ class BhComposer:
 
         return image_bg
 
+    def generate_ants_image(self):
+        white = 255
+
+        image_bg = Image.new("L", (self.background_size, self.background_size), 0)
+        image_ants = Image.new("L", (self.ants_tile_size, self.ants_tile_size), 0)
+        draw_ants = ImageDraw.Draw(image_ants)
+
+        draw_ants.line((-2, 18, 18, -2), fill=white, width=6)
+        draw_ants.line((-6, 6, 6, -6), fill=white, width=6)
+        draw_ants.line((10, 22, 22, 10), fill=white, width=6)
+
+        dr = self.background_size // self.ants_tile_size
+        for i_ in range(0, dr):
+            for j_ in range(0, dr):
+                image_bg.paste(image_ants, (i_ * self.ants_tile_size, j_ * self.ants_tile_size))
+
+        return image_bg
+
     def get_background_tile_image(self):
         return self.background_tile_image
 
@@ -65,6 +87,10 @@ class BhComposer:
     def get_compose_image(self, x1, y1, x2, y2):
         if self.l_image is None:
             return
+
+        self.ants_position += 2
+        if self.ants_position >= 15:
+            self.ants_position = 0
 
         if x1 is None:
             x1 = 0
@@ -98,10 +124,25 @@ class BhComposer:
             if tmp_mask_img.mode != "L":
                 tmp_mask_img.convert("L")
 
-            tmp_mask_img2 = ImageChops.invert(tmp_mask_img)  # We want see mask in this place.
-            tmp_mask_img = ImageChops.multiply(tmp_mask_img2, Image.new("L", (w, h), 128))
+            if False:
+                tmp_mask_img2 = ImageChops.invert(tmp_mask_img)
+                tmp_mask_img = ImageChops.multiply(tmp_mask_img2, Image.new("L", (w, h), 128))
+                tmp_image = Image.new("RGBA", (w, h), (255, 0, 0, 127))
+                image.paste(tmp_image, (0, 0), tmp_mask_img)
+            else:
+                tmp_image = Image.new("RGBA", (w, h), (0, 0, 0, 0))
+                tmp_mask_img2 = ImageChops.invert(tmp_mask_img).filter(ImageFilter.CONTOUR)
+                tmp_mask_img3 = ImageChops.invert(tmp_mask_img2)
 
-            tmp_image = Image.new("RGBA", (w, h), (255, 0, 0, 127))
-            image.paste(tmp_image, (0, 0), tmp_mask_img)
+                tmp_image.paste(tmp_mask_img2, (0, 0), tmp_mask_img3)
+
+                self.ants_image = Image.new("L", (w, h), 0)
+                for i_ in range(0, w, self.background_size):
+                    for j_ in range(0, h - 1, self.background_size):
+                        self.ants_image.paste(self.ants_tile_image, (i_ - self.ants_position, j_))
+
+                tmp_image_3 = tmp_image.convert("L")
+                tmp_image_2 = ImageChops.add(tmp_image_3, self.ants_image)
+                image.paste(tmp_image_2, (0, 0), tmp_image)
 
         return image
